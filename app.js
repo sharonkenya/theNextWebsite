9f39057e145f0e944342bcaee9010c58416b410b
Sharon K <shapzmeloz@gmail.com>
2023-08-24 00:50:30 +0300

    Made multiple changes on the style file

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 const navLogo = document.querySelector('#navbar__logo');
 
-// Display Mobile Menu
-const mobileMenu = () => {
+menu.addEventListener('click', function(){
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
-};
+});
+// Display Mobile Menu
+// const mobileMenu = () => {
+  // menu.classList.toggle('is-active');
+  // menuLinks.classList.toggle('active');
+// };
 
 menu.addEventListener('click', mobileMenu);
 
